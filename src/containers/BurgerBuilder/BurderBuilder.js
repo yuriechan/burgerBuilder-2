16cb560dac90428c338c6b85e246db6e0b1be73d
Yurie <cutecupcakeandcute@gmail.com>
2019-09-05 13:55:37 +0900

    populate result according to the length of the array

--- src/containers/BurgerBuilder/BurderBuilder.js
+++ src/containers/BurgerBuilder/BurderBuilder.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meet: 2,
+      },
+    };
+  }
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Burild Controls</div>
       </Aux>
     );
