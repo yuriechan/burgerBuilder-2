1cdc8307db3c947384d67312e690e4f64bf5e3f1
Yurie <cutecupcakeandcute@gmail.com>
2019-09-06 00:22:38 +0900

    add methods

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,29 @@
 import React from "react";
+import classes from "./BuildControls.css";
 import BuildControl from "./BuildControl/BuildControl";
 
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
 const buildControls = props => {
-  <div></div>;
+  return (
+    <div className={classes.buildControls}>
+      {controls.map(ctrl => {
+        return (
+          <BuildControl
+            added={() => props.ingredientsAdded(ctrl.type)}
+            removed={() => props.ingredientsRemoved(ctrl.type)}
+            key={ctrl.label}
+            label={ctrl.label}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default buildControls;
